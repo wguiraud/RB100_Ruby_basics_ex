50606ca37c2aa8f74df11cc50de8d1783236ca44
LaunchSchool <wguiraud@protonmail.com>
2021-08-23 11:43:22 -0700

    first 'difficult' exercise'

--- Exercises_Ruby_Basics_100/User_Input/Dividing_Numbers_8/dividing_numbers.rb
+++ Exercises_Ruby_Basics_100/User_Input/Dividing_Numbers_8/dividing_numbers.rb
@@ -3,29 +3,34 @@
 # Since this user input, there's good chance that the user won't enter a valid
 # integer. Therefore, you must validate the input to be it is an integer.
 # You can use the following code to determine whether the input is an integer:
-
 def valid_number?(number_string)
 	number_string.to_i.to_s == number_string
 end
 
-
-loop do
-	
+numerator = nil 
+loop do 
 	puts ">> Please enter the numerator:"
-  numerator = gets.chomp
+	numerator = gets.chomp
 
-  puts ">> Please enter the denominator:"
-  denominator = gets.chomp
-  
-	if valid_number?(numerator) == true and valid_number?(denominator) == true
-	  result = numerator / denominator 
-	elsif denominator == 0
-		puts "Invalid input. A denominator of 0 is not allowed"
-	else
-		puts ">> Invalid input. Only integers are allowed."
-	end
-
+	break if valid_number?(numerator)
+	puts "Invalid input. Only integers are allowed."
 end
 
-puts "#{numerator} / #{denominator} is #{result}"
+denominator = nil
+loop do 
+	puts ">> Please enter the denominator:"
+	denominator = gets.chomp
+	
+
+	if denominator == '0'
+		puts "Invalid input. A denominator of 0 is now allowed."
+	else
+	  break if valid_number?(denominator)
+	  puts "Invalid input. Only integers are allowed."
+	end
+end
+
+result = numerator.to_i / denominator.to_i
+puts "#{numerator} / #{denominator.to_i} is #{result}"
+
 
