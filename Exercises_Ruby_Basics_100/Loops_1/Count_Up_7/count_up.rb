468d9956d5b003756eda470cd5d1d7254e0a56ec
LaunchSchool <wguiraud@protonmail.com>
2021-08-27 15:06:24 -0700

    2 more ex from the Loop1 section

---
+++ Exercises_Ruby_Basics_100/Loops_1/Count_Up_7/count_up.rb
@@ -0,0 +1,46 @@
+=begin
+
+The following code outputs a countdown from 10 to 1. Modify the code so that it
+counts from 1 to 10 instead. 
+
+count = 10  
+
+until count == 0
+  puts count 
+	count += 1
+end
+
+#PROBLEM
+- input: 
+- output: numbers from 1 to 10
+- Task:
+
+Questions:
+-
+-
+-
+
+Requirements:
+-
+-
+
+#EXAMPLES/TEST CASES
+
+
+
+
+#ALGORITHM
+- 
+-
+
+=end
+count = 1
+
+until count > 10
+	puts count
+	count += 1
+end
+
+
+
+
---
+++ Exercises_Ruby_Basics_100/Loops_1/Print_Until_8/print_until.rb
@@ -0,0 +1,34 @@
+=begin
+
+Given the array of several numbers below, use an until looop to pring each
+number. 
+
+numbers = [7, 9, 13, 25, 18] => 7 0 13 25 18
+
+#PROBLEM
+- input: an array of integers
+- output: list of integers from the array
+- Task: print out each number from the array
+
+Questions:
+- until loops run as long as they evaluate the condition as false and stop when
+the condition is true
+-
+-
+Requirements:
+-
+-
+#EXAMPLES/TEST CASES
+
+#ALGORITHM
+- 
+-
+
+=end
+numbers = [7, 9, 13, 25, 18]
+count = 0
+
+until count == 1
+	puts numbers
+	count += 1
+end
