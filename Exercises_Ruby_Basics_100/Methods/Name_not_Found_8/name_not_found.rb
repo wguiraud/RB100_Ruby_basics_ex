f60dd7d905b977caa443ba87a9269d900df47e83
LaunchSchool <wguiraud@protonmail.com>
2021-08-30 12:19:53 -0700

    video recording of the day!

--- Exercises_Ruby_Basics_100/Methods/Name_not_Found_8/name_not_found.rb
+++ Exercises_Ruby_Basics_100/Methods/Name_not_Found_8/name_not_found.rb
@@ -2,12 +2,10 @@
 # parameter should default to the string "Bob" if no argument is given. The
 # method's return value should be the value of the argument.
 
-def assign_name(name = "Bob")
-  return name
+def assign_name(name = 'Bob')
+	name
 end
+
 
 puts assign_name('Kevin') == 'Kevin'
 puts assign_name == 'Bob'
-
-
-
