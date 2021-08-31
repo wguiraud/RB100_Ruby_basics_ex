5320fd6c5b15fc4fa26dbfc608f46edba1b59733
LaunchSchool <wguiraud@protonmail.com>
2021-08-31 11:45:09 -0700

    Loops2 ex plus video recording multiply the sum from the Method section

--- Exercises_Ruby_Basics_100/Methods/Multiply_the_Sum_9/multiply_the_sum.rb
+++ Exercises_Ruby_Basics_100/Methods/Multiply_the_Sum_9/multiply_the_sum.rb
@@ -1,14 +1,13 @@
 # Write the following methods so that each output is true. 
 
-def add(a, b)
-	a + b
+def add(num1, num2)
+	num1 + num2
+end
+def multiply(num1, num2)
+	num1 * num2
 end
 
-def multiply(a, b)
-	a * b
-end
 
 puts add(2, 2) == 4
 puts add(5, 4) == 9
-puts multiply(add(2,2), add(5, 4)) == 36
-
+puts multiply(add(2, 2), add(5, 4)) == 36#multiply(4, 9)
