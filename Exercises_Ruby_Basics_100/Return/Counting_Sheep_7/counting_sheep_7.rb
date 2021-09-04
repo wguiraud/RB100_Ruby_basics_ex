71ea7a61609e83c3212ede255731e3c08f10ce80
LaunchSchool <wguiraud@protonmail.com>
2021-09-04 12:45:40 -0700

    video recording Loops1 control the loop plus exercise from the return section

---
+++ Exercises_Ruby_Basics_100/Return/Counting_Sheep_7/counting_sheep_7.rb
@@ -0,0 +1,34 @@
+=begin                                                                                 
+  
+What will the following code print? Why? Don't run it until you've attempted to
+answer. 
+
+def count_sheep
+  5.times do |sheep|
+    puts sheep
+  end
+end
+
+input: the meal method 
+output: the return value of the meal method
+tasks:
+
+questions:
+
+requirements:
+
+ALGORITHM:
+- 
+- 
+
+
+=end
+
+def count_sheep
+	5.times do |sheep|
+	  puts sheep
+	end
+end
+
+count_sheep
+
